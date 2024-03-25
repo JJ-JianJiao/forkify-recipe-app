57307ce56f83d9446d5eb3b498e6b7832031c6e4
Jiao Jian <jj.jianjiao@gmail.com>
2024-03-25 00:50:18 -0400

    fix fraction package error

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,7 @@
 import View from './view.js';
 import icons from 'url:../../img/icons.svg';
 import {Fraction} from 'fractional';
+import {numberToFraction} from "../helpers.js";
 
 class RecipeView extends View{
     _parentElement = document.querySelector('.recipe');
@@ -116,7 +117,7 @@ class RecipeView extends View{
                 <svg class="recipe__icon">
                 <use href="${icons}#icon-check"></use>
                 </svg>
-                <div class="recipe__quantity">${ing.quantity ? new Fraction(+ing.quantity).toString() : ""}</div>
+                <div class="recipe__quantity">${ing.quantity ? numberToFraction(+ing.quantity).toString() : ""}</div>
                 <div class="recipe__description">
                 <span class="recipe__unit">${ing.unit}</span>
                 ${ing.description}
